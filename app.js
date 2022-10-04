b042824cf5499cce048bd30c11c878e681ca53eb
Bunyodbek8272 <you@example.com>
2022-10-04 13:34:35 +0500

    counter

---
+++ app.js
@@ -0,0 +1,20 @@
+//Set initial count
+let count = 0;
+//select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(btn => {
+    btn.addEventListener("click", function(e){
+        const style = e.currentTarget.classList;
+        if(style.contains('decrease')){
+            count--;
+        }else if(style.contains('increase')){
+            count++;
+        }else{
+            count = 0;
+        }
+        value.textContent =  count;
+    
+    })
+});
